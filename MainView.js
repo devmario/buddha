0777c9c30dbecfc4461c60e29aa84aa20f3280f8
jangwonhee <jangwonhee@jangwonheeui-Mac-Pro.local>
2015-10-14 20:53:44 +0900

    animation page

--- MainView.js
+++ MainView.js
@@ -7,20 +7,18 @@ Help Page
 */
 
 var animSRC = Observable("res/img/kid00.png");
-var animText = Observable("");
+var animText0 = Observable(false);
+var animText1 = Observable(false);
+var animText2 = Observable(false);
+var animText3 = Observable(false);
+var animText4 = Observable(false);
+var animText5 = Observable(false);
+var animText6 = Observable(false);
+var animText7 = Observable(false);
 var help2NaviAlpha0 = Observable(0.5);
 var help2NaviAlpha1 = Observable(1.0);
 var help2NaviAlpha2 = Observable(1.0);
-var animTextArr = [
-	"마음을 차분하게 가라앉히고 두 손을 가슴에 모으며 바르게 섭니다. 이것을 합장이라 합니다.",
-	"합장을 한 채 인사를 하듯 천천히 허리를 숙였다 세웁니다. 이것을 반배라하는데 절을시작할때와 끝날 때에는 항상 반배를 합니다.",
-	"두손을 가슴에 모은 채 허리를 살짝 굽히면서 무릎을 꿇으며 앉습니다. 자세가 흐트러지면 넘어질 수 있으니 천천히 중심을 잡으며 앉습니다.",
-	"엉덩이와 발 뒤꿈치가 맞닿을 때쯤 상체를 앞으로 숙이면서 오른손을 바닥에 내려놓습니다. 발은 자연스럽게 오른발이 왼발 위로 올라가게 합니다. 오른손과 나란히 왼손을 내려놓으며 이마가 바닥에 닿을 정도로 숙입니다. 이처럼 이마, 양쪽 팔꿈치와 무릎이 모두 바닥에 닿은 것을 오체투지라 합니다.",
-	"양쪽 팔꿈치를 바닥에 댄 채 두 손바닥이 하늘을 향하도록 뒤집으면서 양쪽 귀옆으로 살짝 들어올립니다. 이것은 나 자신을 낮추고 세상 모든 존재를 지극한 마음으로 정성을 다해 존중하겠다는 다짐입니다.",
-	"다시 두 손을 바닥을 향해 뒤집어 머리 옆으로 가지런히 내려놓으며 오체투지 자세를 취합니다.",
-	"고개와 허리를 곧은 자세로 들면서 왼손과 오른손을 차례로 가슴 쪽으로 모읍니다. 이렇게 하면 합장하며 앉은 자세가 됩니다.",
-	"포게 놓았던 양 발을 가지런히 풀면서 엉덩이를 살짝 들어올리며 일어납니다. 절을 시작할 때의 자세처럼 양쪽발은 가지런히 모으고 두 손은 가슴쪽으로 모아 바르게 섭니다. 이 자세에서 무릎을 굽히며 다시 절하는 동작을 반복합니다. 108번째 절을 마쳤을 때엔 반 배를 하며 절 수행을 마무리합니다."
-];
+
 var animFrame = 0;
 var animTo = 0;
 var animSpeed = 1000/5;
@@ -29,8 +27,13 @@ var animIndex = 0;
 
 var timeout = null;
 
+var frameCheck = Observable(false);
+var frame = Observable({key:"kid0"});
+
 function animCommit() {
-	animSRC.value = "res/img/kid" + ("0" + animFrame).slice(-2)+ ".png";
+	frameCheck.value = false;
+	frame.value.key = "kid" + animFrame;
+	frameCheck.value = true;
 }
 
 function animationStart() {
@@ -47,12 +50,20 @@ function animationStart() {
 	if(animFrame != animTo) {
 		timeout = setTimeout(animationStart, animSpeed);
 	} else {
-		help2NaviAlpha1.value = 1.0;
+		if(animIndex == 0) {
+			help2NaviAlpha1.value = 0.0;
+		} else {
+			help2NaviAlpha1.value = 1.0;
+		}
 	}
 }
 
 function animUpdate() {
-	help2NaviAlpha1.value = 0.5;
+	if(animIndex == 0) {
+		help2NaviAlpha1.value = 0.0;
+	} else {
+		help2NaviAlpha1.value = 0.5;
+	}
 	if(animIndex > 0) {
 		animFrame = animSequence[animIndex - 1];
 		animTo = animSequence[animIndex];
@@ -64,7 +75,7 @@ function animUpdate() {
 }
 
 function animGoToPlaySequence(index) {
-	if(index > animTextArr.length - 1)
+	if(index > animSequence.length - 1)
 		return;
 	if(index < 0)
 		return;
@@ -75,14 +86,22 @@ function animGoToPlaySequence(index) {
 	help2NaviAlpha0.value = 1.0;
 	help2NaviAlpha1.value = 1.0;
 	help2NaviAlpha2.value = 1.0;
-	if(index == 0)
+	if(index == 0) {
 		help2NaviAlpha0.value = 0.5;
-	if(index == animTextArr.length - 1)
+	}
+	if(index == animSequence.length - 1)
 		help2NaviAlpha2.value = 0.5;
 
 	animUpdate();
 
-	animText.value = animTextArr[index];
+	animText0.value = index == 0;
+	animText1.value = index == 1;
+	animText2.value = index == 2;
+	animText3.value = index == 3;
+	animText4.value = index == 4;
+	animText5.value = index == 5;
+	animText6.value = index == 6;
+	animText7.value = index == 7;
 }
 
 
@@ -111,12 +130,23 @@ Module
 module.exports = {
 	animSRC: animSRC,
 	help2Active: help2Active,
-	animText: animText,
+
+	animText0: animText0,
+	animText1: animText1,
+	animText2: animText2,
+	animText3: animText3,
+	animText4: animText4,
+	animText5: animText5,
+	animText6: animText6,
+	animText7: animText7,
+	
 	help2NaviPrev: help2NaviPrev,
 	help2NaviPlay: help2NaviPlay,
 	help2NaviNext: help2NaviNext,
 	help2NaviAlpha0: help2NaviAlpha0,
 	help2NaviAlpha1: help2NaviAlpha1,
-	help2NaviAlpha2: help2NaviAlpha2
+	help2NaviAlpha2: help2NaviAlpha2,
+	frameCheck: frameCheck,
+	frame: frame
 };
 
